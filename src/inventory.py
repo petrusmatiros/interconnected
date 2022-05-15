2fe53ff8c9247f3e0b939e78743bb90f1e28145e
Petrus Matiros <petrus.matiros@gmail.com>
2022-05-15 11:44:30 +0200

    Add key and inventory class

--- src/inventory.py
+++ src/inventory.py
@@ -1,1 +1,39 @@
+from key import *
+class Inventory():
+    """
+    Class to represent the inventory of a character.
+    """
+    def __init__(self):
+        """Initializes a new Inventory object
+        """
+        self.keys = []
+        self.max_red = 3
+        self.max_purple = 1
+        
+    def add_key(self, keycolor):
+        """Adds a key to the inventory
+        """
+        if keycolor == Color.RED:
+            if self.get_amount(keycolor) < self.max_red:
+                self.keys.append(keycolor)
+        elif keycolor == Color.PURPLE:
+            if self.get_amount(keycolor) < self.max_purple:
+                self.keys.append(keycolor)
+                
+    def get_amount(self, keycolor):
+        """Returns the amount of the specified key
+        """
+        key_count = 0
+        
+        if len(self.keys) == 0:
+            return 0
+        
+        for key in self.keys:
+            if key == keycolor:
+                key_count += 1
+        
+        return key_count
 
+        
+        
+        
--- src/key.py
+++ src/key.py
@@ -1,1 +1,24 @@
+from enum import Enum
+class Color(Enum):
+    RED = "Red"
+    PURPLE = "Purple"
 
+class Key():
+    """
+    Class to represent the keys in the game
+    """
+    def __init__(self, color, description):
+        """Initializes a new Key object
+		"""
+        self.color = color
+        self.description = description
+
+    def get_color(self):
+        """Returns the color of the key
+		"""
+        return self.color
+    
+    def get_description(self):
+        """Returns the description of the key
+		"""
+        return self.description
